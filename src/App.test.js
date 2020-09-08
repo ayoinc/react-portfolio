cb621ed0842481ed96ce301bf4bc52ef60d96416
Ayo <ayoinc@users.noreply.github.com>
2020-09-08 17:12:37 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
-import ReactDOM from 'react-dom';
 import App from './App';
-
-Sentry.init({
-  dsn: "https://71e1519d2f384d80a0b80c01151ffa14@o352413.ingest.sentry.io/5421638",
-  integrations: [
-    new Integrations.BrowserTracing(),
-  ],
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
