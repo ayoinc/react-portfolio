0e80d8456941796dfeb0163c56d062c6eaf7bfe0
Ayo <ayo@iMac.local>
2020-09-09 21:33:59 +0100

    credit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,14 +18,14 @@ class Footer extends Component {
               {networks}
            </ul>
 
-           <ul>
+           {/* <ul>
             <li>Modified with love by the Clever Programmer team</li>
-           </ul>
+           </ul> */}
 
-           <ul className="copyright">
+           {/* <ul className="copyright">
               <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
+           </ul> */}
 
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
