f94ed32ec1359f29ddae85fc74223df74201f8fa
Ayo <ayoinc@users.noreply.github.com>
2021-01-12 10:45:28 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-<!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "e46c2b3a95844bc2a20b58c973bdd8c3"}'></script><!-- End Cloudflare Web Analytics -->
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
+<!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "e46c2b3a95844bc2a20b58c973bdd8c3"}'></script><!-- End Cloudflare Web Analytics -->
