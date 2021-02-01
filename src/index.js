c79b11ca40b1d0772024c0ac99ed472f934d3b77
Ayo <ayoinc@gmail.com>
2021-02-01 15:41:05 +0000

    hemet removed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {Helmet} from 'react-helmet';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
