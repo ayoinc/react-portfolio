d13656ca42ca76aaf59dddc7381ba3924bae684e
Ayo <ayoinc@gmail.com>
2021-02-01 15:02:39 +0000

    adding sc

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "e46c2b3a95844bc2a20b58c973bdd8c3"}'></script>
 registerServiceWorker();
 
