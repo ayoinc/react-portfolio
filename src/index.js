946928a522f98445fc2fd613405f8e7f02635d2e
Ayo <ayoinc@gmail.com>
2021-02-01 15:31:58 +0000

    remove hemet lines

--- src/index.js
+++ src/index.js
@@ -7,17 +7,6 @@ import {Helmet} from 'react-helmet';
 
 
 
-	render() {
-	  return (
-	  	<div className="application">
-		  <Helmet>
-		  	<meta charset="utf-8" />
-		  	<title>CloudFlare</title>
-		  	<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "e46c2b3a95844bc2a20b58c973bdd8c3"}'></script>
-		  </Helmet>
-		</div>
-	  )
-	}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
